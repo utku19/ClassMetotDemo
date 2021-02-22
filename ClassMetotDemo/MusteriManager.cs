99621042499d3dc76310495aba20d2c51f167a35
utku berk gök <utku_19_berk@hotmail.com>
2021-02-22 15:36:44 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("\n Id No : " + musteri.Id + " \n Adı : " + musteri.Adi + " \n Soyadı : " + musteri.Soyadi + " \n Yaş : " + musteri.Yas + " \n Aylık Süre : " + musteri.KullanimSuresiAy);
+            Console.WriteLine("Müşteri eklendi");
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine(" \n Id No : " + musteri.Id + " \n Adı : " + musteri.Adi + " \n Soyadı : " + musteri.Soyadi + " \n Yaş : " + musteri.Yas + " \n Aylık Süre : " + musteri.KullanimSuresiAy);
+            Console.WriteLine("Müşteri Silindi");
+        }
+
+        public void Liste(Musteri[] musteriler)
+        {
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(" \n Id No : " + musteri.Id + " \n Adı : " + musteri.Adi + " \n Soyadı : " + musteri.Soyadi + " \n Yaş : " + musteri.Yas + " \n Aylık Süre : " + musteri.KullanimSuresiAy);
+            }
+            Console.WriteLine("Tüm müşteri bilgileri");
+
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1221321;
+            musteri1.Adi = "Utku";
+            musteri1.Soyadi = "Gök";
+            musteri1.Yas = 24;
+            musteri1.KullanimSuresiAy = 24;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 1312325;
+            musteri2.Adi = "Tutku";
+            musteri2.Soyadi = "Gök";
+            musteri2.Yas = 31;
+            musteri2.KullanimSuresiAy = 36;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 1341214;
+            musteri3.Adi = "Ataberk";
+            musteri3.Soyadi = "Kurumoğlu";
+            musteri3.Yas = 24;
+            musteri3.KullanimSuresiAy = 12;
+
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+            MusteriManager musteriManager = new MusteriManager();
+
+            musteriManager.Liste(musteriler);
+            musteriManager.Ekle(musteri2);
+            musteriManager.Sil(musteri1);
+
+            
+            
+
+        }
+    }
+}
